b0d3ed69ccc17c8a9bbd4c7356c89009210698d2
alexander-sysuiev <alexander.sysuiev@gmail.com>
2018-08-23 20:22:51 +0300

    Fix chrome extension element select

--- plugins/browsers/chrome/extension.js
+++ plugins/browsers/chrome/extension.js
@@ -1,28 +1,27 @@
-const trueautomationLocalIdeServerUrl = 'http://localhost:9898';
-const trueautomationDevIdeServerUrl = 'http://app-dev.trueautomation.io';
-const trueautomationProdIdeServerUrl = 'https://trueautomation.io/';
-
 function executeExtension() {
   let taScript;
+  const trueautomationLocalIdeServerUrl = 'http://localhost:3000';
+  const trueautomationDevIdeServerUrl = 'http://app-dev.trueautomation.io';
+  const trueautomationProdIdeServerUrl = 'https://trueautomation.io/';
 
   chrome.storage.local.get('taScript', function(result) {
-    taScript = result;
-  });
+    taScript = result['taScript'];
 
-  if (!taScript) {
-    fetch(`${trueautomationDevIdeServerUrl}/client/ide/getPlugin`).then(function (response) {
-      return response.text();
-    }).then(function(respBody) {
-      taScript = respBody;
-      chrome.storage.local.set({ 'taScript': taScript })
+    if (!taScript) {
+      fetch(`${trueautomationLocalIdeServerUrl}/client/ide/getPlugin`).then(function (response) {
+        return response.text();
+      }).then(function(respBody) {
+        taScript = respBody;
+        chrome.storage.local.set({ 'taScript': taScript })
+        eval(taScript);
+
+      }).catch(function (err) {
+        console.log('Error occurred:', err);
+      });
+    } else {
       eval(taScript);
-
-    }).catch(function (err) {
-      console.log('Error occurred:', err);
-    });  
-  } else {
-    eval(taScript);
-  }
+    }
+  });
 }
 
 executeExtension();
