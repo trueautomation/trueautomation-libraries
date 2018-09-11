1820110da15ac4e9d2f28f39c4eebc0c2f923d31
alexander-sysuiev <alexander.sysuiev@gmail.com>
2018-09-11 16:48:38 +0300

    Fixes on ide server automatic run

--- plugins/ide/trueautomation-atom/lib/trueautomation-atom-provider.js
+++ plugins/ide/trueautomation-atom/lib/trueautomation-atom-provider.js
@@ -1,6 +1,7 @@
 'use babel';
 
 import fetch from 'isomorphic-fetch';
+import { exec } from 'child_process';
 
 const TA_REGEX = /ta\s{0,}\(\s{0,}[\'\"]/;
 
@@ -12,6 +13,27 @@ class TrueautomationAtomProvider {
     this.filterSuggestions = true;
   }
 
+  isRunning(processName) {
+    return new Promise((resolve, reject) => {
+      let platform = process.platform;
+      let cmd = '';
+      switch (platform) {
+          case 'win32' : cmd = `tasklist`; break;
+          case 'darwin' : cmd = `ps -ax | grep "${processName}"`; break;
+          case 'linux' : cmd = `ps -A`; break;
+          default: break;
+      }
+
+      exec(cmd, (err, stdout, stderr) => {
+        const processList = stdout.toLowerCase().
+                            replace(/grep "trueautomation ide"/, '').
+                            replace(/grep trueautomation ide/, '');
+
+        resolve(processList.indexOf(processName.toLowerCase()) > -1)
+      })
+    })
+  }
+
   async getSuggestions(params) {
     const {
       editor,
@@ -21,9 +43,10 @@ class TrueautomationAtomProvider {
       activatedManually,
     } = params;
 
+    const isIdeRun = await this.isRunning('trueautomation ide');
     const line = editor.getTextInRange([[bufferPosition.row, 0], bufferPosition]);
 
-    if (line.match(TA_REGEX)) {
+    if (isIdeRun && line.match(TA_REGEX)) {
       const elementJson = await fetch(`http://localhost:9898/ide/searchElements?query=${prefix}`, {
         method: 'GET',
       });
