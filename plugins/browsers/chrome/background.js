c46199757bf2cb7398b972b736bfb5f86216123a
artemdordovskyi <dordovskyartem@gmail.com>
2018-10-19 13:32:16 +0500

    [TA-491] Fix extension on sites with ssl.

--- plugins/browsers/chrome/background.js
+++ plugins/browsers/chrome/background.js
@@ -1,3 +1,19 @@
+chrome.webRequest.onHeadersReceived.addListener(
+  function (details) {
+    for (let i = 0; i < details.responseHeaders.length; ++i) {
+      if ('x-frame-options' === details.responseHeaders[i].name.toLowerCase()) {
+        details.responseHeaders.splice(i, 1);
+      }
+      if ('content-security-policy' === details.responseHeaders[i].name.toLowerCase()) {
+        details.responseHeaders[i].value = '';
+      }
+    }
+    return {
+      responseHeaders: details.responseHeaders
+    };
+  }, {
+    urls: ["<all_urls>"]
+  }, ["blocking", "responseHeaders"]);
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
