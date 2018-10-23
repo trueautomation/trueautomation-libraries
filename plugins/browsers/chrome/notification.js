7bc831ea8b97e5695ad98310144e1e77ff0fa78a
artemdordovskyi <dordovskyartem@gmail.com>
2018-10-23 11:40:59 +0500

    Fixes

--- plugins/browsers/chrome/notification.js
+++ plugins/browsers/chrome/notification.js
@@ -13,17 +13,18 @@ function executeExtension() {
   let div = document.createElement("div");
   document.body.appendChild(div);
   div.innerHTML = `Element locator has been <span style="color:#ee6c4d;">successfully</span> saved.`;
-  div.style.position = "absolute";
+  div.style.position = "fixed";
   div.style.top = "10px";
   div.style.right = "10px";
   div.style.padding = "24px 36px";
   div.style.borderRadius = "10px";
   div.style.border = "1px solid #ee6c4d";
   div.style.backgroundColor = "#fff";
-  div.style.zIndex = maxZIndex + 101;
+  div.style.zIndex = maxZIndex*10 + 1;
   div.style.fontSize = "16px";
   div.style.letterSpacing = "0.3px";
   div.style.fontFamily = "Montserrat-SemiBold, sans-serif";
+  div.style.color = "#000";
 
   setTimeout(() => { div.remove(); link.remove(); }, 3000);
 }
