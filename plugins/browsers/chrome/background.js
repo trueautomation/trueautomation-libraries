e4d662483f40abbab6507c1557329d421b0fb3c7
artemdordovskyi <dordovskyartem@gmail.com>
2019-01-08 11:46:11 +0200

    [TA-606] Fix screenshots by context menu

--- plugins/browsers/chrome/background.js
+++ plugins/browsers/chrome/background.js
@@ -34,3 +34,15 @@ chrome.contextMenus.create({
     chrome.tabs.sendMessage(tab.id, {});
   }
 });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    chrome.tabs.captureVisibleTab(
+      null,
+      {},
+      function(dataUrl){
+        sendResponse({imgSrc: dataUrl});
+      }); //remember that captureVisibleTab() is a statement
+    return true;
+  }
+);
